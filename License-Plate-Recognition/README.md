e1b885b8c7f45ead22653c8b24243d6e6be82eb5
Phạm Nguyễn Bảo Trang <167573896+TinoDragna@users.noreply.github.com>
2025-12-24 14:31:54 +0700

    use extend cam

--- License-Plate-Recognition/README.md
+++ License-Plate-Recognition/README.md
@@ -1,21 +1,10 @@
 ```bash
+
+environment run: >= python 3.11.0
 pip install -r ./requirement.txt
-python plate_scanner        (return lp path, img_path, just_LP, plate = scan_plate())
+python subscribe_addLPR_Face.py
+#ctrl+c to exit
 
-
-pip install pymysql
-pip install paho-mqtt
-
-
-//-----------------------------------------------------
-pip install mysql-connector-python (nếu cần làm việc với database)
-
-
-//-----------------------------------------------------
-python webcam.py 
-python webcam2.py 
-
-python upWeb.py             #ctrl+c to exit
 //-----------------------------------------------------
 
 Read Note.md to know more
--- License-Plate-Recognition/camera.py
+++ License-Plate-Recognition/camera.py
@@ -1,37 +1,25 @@
 # camera.py
 import cv2
 import threading
-
-# class Camera:
-#     def __init__(self, src=0):
-#         self.cap = cv2.VideoCapture(src, cv2.CAP_DSHOW)
-#         self.lock = threading.Lock()
-
-#         if not self.cap.isOpened():
-#             raise RuntimeError("❌ Cannot open camera")
-
-#     def read(self):
-#         with self.lock:
-#             ret, frame = self.cap.read()
-#         return ret, frame
-
-#     def release(self):
-#         with self.lock:
-#             if self.cap.isOpened():
-#                 self.cap.release()
-
-# # GLOBAL SINGLETON
-# camera = Camera(0)
+import time
 
 class CameraService:
-    def __init__(self, cam_id=0):
-        self.cap = cv2.VideoCapture(cam_id)
+    def __init__(self, cam_id=0):   #0 là cam máy, 1 là ngoại vi
+        self.cap = cv2.VideoCapture(cam_id, cv2.CAP_DSHOW)
         if not self.cap.isOpened():
             raise RuntimeError("Camera open failed")
+        self.lock = threading.Lock()
 
     def read(self):
-        return self.cap.read()
+        with self.lock:
+            ret, frame = self.cap.read()
+            if not ret:
+                print("⚠️ Camera read failed")
+                time.sleep(0.2)
+            return ret, frame
 
     def release(self):
-        self.cap.release()
+        with self.lock:
+            self.cap.release()
+
 camera = CameraService(0)
