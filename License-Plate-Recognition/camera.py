61270a5b66609b738c9f8aaaed1f9752ea2c6653
Phạm Nguyễn Bảo Trang <167573896+TinoDragna@users.noreply.github.com>
2025-12-24 15:13:50 +0700

    use usb cam

--- License-Plate-Recognition/camera.py
+++ License-Plate-Recognition/camera.py
@@ -1,25 +1,37 @@
 # camera.py
 import cv2
 import threading
-import time
+
+# class Camera:
+#     def __init__(self, src=0):
+#         self.cap = cv2.VideoCapture(src, cv2.CAP_DSHOW)
+#         self.lock = threading.Lock()
+
+#         if not self.cap.isOpened():
+#             raise RuntimeError("❌ Cannot open camera")
+
+#     def read(self):
+#         with self.lock:
+#             ret, frame = self.cap.read()
+#         return ret, frame
+
+#     def release(self):
+#         with self.lock:
+#             if self.cap.isOpened():
+#                 self.cap.release()
+
+# # GLOBAL SINGLETON
+# camera = Camera(0)
 
 class CameraService:
-    def __init__(self, cam_id=0):   #0 là cam máy, 1 là ngoại vi
+    def __init__(self, cam_id=0):
         self.cap = cv2.VideoCapture(cam_id, cv2.CAP_DSHOW)
         if not self.cap.isOpened():
             raise RuntimeError("Camera open failed")
-        self.lock = threading.Lock()
 
     def read(self):
-        with self.lock:
-            ret, frame = self.cap.read()
-            if not ret:
-                print("⚠️ Camera read failed")
-                time.sleep(0.2)
-            return ret, frame
+        return self.cap.read()
 
     def release(self):
-        with self.lock:
-            self.cap.release()
-
+        self.cap.release()
 camera = CameraService(0)
